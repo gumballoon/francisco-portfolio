69f0366cad7da7d75839972d72c32cacac2eac87
Francisco Cristina <franciscocristina.sound@gmail.com>
2025-10-03 09:48:20 +0100

    add updated photos

--- src/SkillsIcon.jsx
+++ src/SkillsIcon.jsx
@@ -5,7 +5,7 @@ export default function SkillsIcon({ SVG, width, description } ) {
         <div className='SkillsIcon flex flex-col items-center' role='img' aria-label={description}>
             < SVG className={`w-[${width}] max-w-12 peer`} aria-hidden='true'/>
 
-        <span className='block mt-2 font-extralight opacity-0 peer-hover:opacity-100 transition-opacity duration-300' aria-hidden='true'>{description}</span>
+        <span className='block mt-2 font-extralight opacity-0 peer-hover:opacity-100 peer-focus:opacity-100 transition-opacity duration-300' aria-hidden='true'>{description}</span>
     </div>
     )
 } 
